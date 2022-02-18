05dda7c531c5c7ecdded6d45533e73f852db3043
khieutrung12 <khieutrung12@gmail.com>
2022-02-18 11:54:46 +0700

    localization (i18n)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LanguageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/lang/{locale}', [LanguageController::class, 'switchLanguage'])->name('lang');
