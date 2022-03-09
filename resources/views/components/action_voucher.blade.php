86f55ee74debcf0ce50219f9e66c4df429cc51b3
DTHNhung <damthihongnhung2ktn@gmail.com>
2022-03-09 17:52:44 +0700

    [Admin] View list vouchers

---
+++ resources/views/components/action_voucher.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <button class='btn btn-sm' data-id="{{ $id }}" id='editVoucherBtn'>
+        <i class='fa-solid fa-pen-to-square text-success'></i>
+    </button>
+    <button class='btn btn-sm' data-id="{{ $id }}" id='deleteVoucherBtn'>
+        <i class='fa-solid fa-trash text-danger'></i>
+    </button>
+</div>
